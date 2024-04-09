9cebf3813bce95680c3026fa817d1ef73f7a782e
David Ly <dly@horizontal.com>
2024-04-09 15:31:12 -0500

    Updated Layout to use OOTB placeholder/layout with custom wrapper.

---
+++ src/hztl-foundation/src/components/authorable/shared/site-structure/main-layout/MainLayout.tsx
@@ -0,0 +1,20 @@
+import { Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
+
+import { ComponentProps } from 'lib/component-props';
+
+export const Default = (props: ComponentProps): JSX.Element => {
+  const id = props?.params?.RenderingIdentifier;
+  return (
+    <div className="w-full" style={{ width: '100%' }} data-component="main-layout" id={id}>
+      <div>
+        <Placeholder name="hztl-headless-breadcrumb" rendering={props.rendering} />
+      </div>
+      <div>
+        <Placeholder name="hztl-headless-hero" rendering={props.rendering} />
+      </div>
+      <div>
+        <Placeholder name="hztl-headless-main-content" rendering={props.rendering} />
+      </div>
+    </div>
+  );
+};
