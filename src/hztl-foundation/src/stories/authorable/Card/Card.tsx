f1bca4b5ddc3ca0fccb240c89981f1b4ae9f2297
ShivamKuSingh <ssingh@horizontal.com>
2024-05-24 19:21:41 +0530

    Build issue resolved of card component

--- src/hztl-foundation/src/stories/authorable/Card/Card.tsx
+++ src/hztl-foundation/src/stories/authorable/Card/Card.tsx
@@ -28,49 +28,49 @@ const Card = (props: CardProps): JSX.Element => (
       </div>
       <div className="flex justify-center items-center w-[450px] h-[315px] p-[40px]">
         <div>
-        <div>
-          <Text
-            className="font-mordern text-[12px] font-[400] not-italic leading-normal mb-[8px]"
-            field={props.fields.eyebrow}
-            tag="h6"
-            editable
-          />
-        </div>
-        <div>
-          <Text
-            className="font-mordern text-[36px] font-[700] not-italic leading-normal mb-[8px]"
-            field={props.fields.heading}
-            tag="h1"
-            editable
-          />
-        </div>
-        <div>
-          <Text
-            className="font-mordern text-[20px] font-[700] not-italic leading-normal mb-[8px]"
-            field={props.fields.subHeading}
-            tag="h3"
-            editable
-          />
-        </div>
-        <div>
-          <Text
-            className="font-mordern text-[16px] font-[400] not-italic leading-[24px] mb-[8px]"
-            field={props.fields.description}
-            tag="p"
-            editable
-          />
-        </div>
-        <div className="flex gap-[8px]">
-          <LinkWrapper
-            className="w-[140px] h-[48px] px-[16px] py-[12px] rounded-[4px] bg-[#2F2D2E] text-center text-[#FFF] font-mordern text-[14px] font-[700] not-italic leading-normal"
-            field={props.fields.learnMoreBtn}
-          />
+          <div>
+            <Text
+              className="font-mordern text-[12px] font-[400] not-italic leading-normal mb-[8px]"
+              field={props.fields.eyebrow}
+              tag="h6"
+              editable
+            />
+          </div>
+          <div>
+            <Text
+              className="font-mordern text-[36px] font-[700] not-italic leading-normal mb-[8px]"
+              field={props.fields.heading}
+              tag="h1"
+              editable
+            />
+          </div>
+          <div>
+            <Text
+              className="font-mordern text-[20px] font-[700] not-italic leading-normal mb-[8px]"
+              field={props.fields.subHeading}
+              tag="h3"
+              editable
+            />
+          </div>
+          <div>
+            <Text
+              className="font-mordern text-[16px] font-[400] not-italic leading-[24px] mb-[8px]"
+              field={props.fields.description}
+              tag="p"
+              editable
+            />
+          </div>
+          <div className="flex gap-[8px]">
+            <LinkWrapper
+              className="w-[140px] h-[48px] px-[16px] py-[12px] rounded-[4px] bg-[#2F2D2E] text-center text-[#FFF] font-mordern text-[14px] font-[700] not-italic leading-normal"
+              field={props.fields.learnMoreBtn}
+            />
 
-          <LinkWrapper
-            className="w-[150px] h-[48px] p-[12px] rounded-[4px] border-[1px] border-[#2F2D2E] text-center text-[#2F2D2E] font-mordern text-[16px] font-[700] not-italic leading-normal"
-            field={props.fields.downloadBtn}
-          />
-        </div>
+            <LinkWrapper
+              className="w-[150px] h-[48px] p-[12px] rounded-[4px] border-[1px] border-[#2F2D2E] text-center text-[#2F2D2E] font-mordern text-[16px] font-[700] not-italic leading-normal"
+              field={props.fields.downloadBtn}
+            />
+          </div>
         </div>
       </div>
     </div>
