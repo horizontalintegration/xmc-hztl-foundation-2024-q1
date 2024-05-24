2df4872be8962b6fc25da20d40d871834cd47681
ShivamKuSingh <ssingh@horizontal.com>
2024-05-24 18:43:07 +0530

    Card Component Completed

---
+++ src/hztl-foundation/src/stories/authorable/Card/Card.mock-data.ts
@@ -0,0 +1,47 @@
+import { CardProps } from './Card';
+
+// Realistically this is going to have a lot more data from Sitecore or XM Cloud, but this is a good example mock data set.
+const defaultData: CardProps = {
+  rendering: { componentName: 'Card' },
+  params: {},
+  fields: {
+    eyebrow: {
+      value: 'Eyebrow',
+    },
+    cardImage: {
+        value: '<svg width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="Icon/Solid/photograph" opacity="0.3"><path id="Icon" fill-rule="evenodd" clip-rule="evenodd" d="M9.59999 7.70001C6.94902 7.70001 4.79999 9.84905 4.79999 12.5V36.5C4.79999 39.151 6.94902 41.3 9.59999 41.3H38.4C41.051 41.3 43.2 39.151 43.2 36.5V12.5C43.2 9.84905 41.051 7.70001 38.4 7.70001H9.59999ZM38.4 36.5H9.59999L19.2 17.3L26.4 31.7L31.2 22.1L38.4 36.5Z" fill="#2F2D2E" /></g></svg>',
+    },
+    heading: {
+      value: 'Services',
+    },
+    subHeading: {
+      value: 'Subheading',
+    },
+    description: {
+      value:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    },
+    learnMoreBtn: {
+      value: {
+        href: 'https://www.horizontaldigital.com',
+        text: 'Learn More',
+        linktype: 'external',
+      },
+    },
+    downloadBtn: {
+      value: {
+        href: 'https://www.horizontaldigital.com',
+        text: 'Download',
+        linktype: 'external',
+      },
+    },
+  },
+};
+
+export const noData = {
+  rnder: {},
+  params: [],
+};
+
+export default defaultData;
+
---
+++ src/hztl-foundation/src/stories/authorable/Card/Card.tsx
@@ -0,0 +1,80 @@
+import { Field, Text, LinkField, LinkFieldValue } from '@sitecore-jss/sitecore-jss-nextjs';
+import { ComponentProps } from 'lib/component-props';
+import RichTextWrapper from 'helpers/SitecoreWrappers/RichTextWrapper/RichTextWrapper';
+import LinkWrapper from 'helpers/LinkWrapper/LinkWrapper';
+
+// Ideally, all this is from generated Typescript code from Sitecore and we're not manually defining types.
+interface Fields {
+  heading: Field<string>;
+  eyebrow: Field<string>;
+  subHeading: Field<string>;
+  description: Field<string>;
+  learnMoreBtn: LinkField | LinkFieldValue;
+  downloadBtn: LinkField | LinkFieldValue;
+  cardImage: Field<string>;
+}
+
+export type CardProps = ComponentProps & {
+  rendering: { componentName: string };
+  params: { [key: string]: string };
+  fields: Fields;
+};
+
+const Card = (props: CardProps): JSX.Element => (
+  <div data-component="authorable/general/card" className="flex justify-center items-center">
+    <div className="h-[614px] w-[450px] border-[1px] border-black">
+      <div className="h-[299px] w-[450px] border-b-[1px] border-black flex justify-center items-center">
+        <RichTextWrapper field={props.fields.cardImage} editable />
+      </div>
+      <div className="flex justify-center items-center w-[450px] h-[315px] p-[40px]">
+        <div>
+        <div>
+          <Text
+            className="font-mordern text-[12px] font-[400] not-italic leading-normal mb-[8px]"
+            field={props.fields.eyebrow}
+            tag="h6"
+            editable
+          />
+        </div>
+        <div>
+          <Text
+            className="font-mordern text-[36px] font-[700] not-italic leading-normal mb-[8px]"
+            field={props.fields.heading}
+            tag="h1"
+            editable
+          />
+        </div>
+        <div>
+          <Text
+            className="font-mordern text-[20px] font-[700] not-italic leading-normal mb-[8px]"
+            field={props.fields.subHeading}
+            tag="h3"
+            editable
+          />
+        </div>
+        <div>
+          <Text
+            className="font-mordern text-[16px] font-[400] not-italic leading-[24px] mb-[8px]"
+            field={props.fields.description}
+            tag="p"
+            editable
+          />
+        </div>
+        <div className="flex gap-[8px]">
+          <LinkWrapper
+            className="w-[140px] h-[48px] px-[16px] py-[12px] rounded-[4px] bg-[#2F2D2E] text-center text-[#FFF] font-mordern text-[14px] font-[700] not-italic leading-normal"
+            field={props.fields.learnMoreBtn}
+          />
+
+          <LinkWrapper
+            className="w-[150px] h-[48px] p-[12px] rounded-[4px] border-[1px] border-[#2F2D2E] text-center text-[#2F2D2E] font-mordern text-[16px] font-[700] not-italic leading-normal"
+            field={props.fields.downloadBtn}
+          />
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Card;
