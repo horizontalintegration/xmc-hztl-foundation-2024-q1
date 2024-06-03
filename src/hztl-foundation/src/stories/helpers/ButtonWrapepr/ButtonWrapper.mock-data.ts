59e554d69b5af4d7cd02c6da07953f733460af1a
Sa'ad <patelsaad39@gmail.com>
2024-06-03 15:54:07 +0530

    mock update

--- src/hztl-foundation/src/stories/helpers/ButtonWrapepr/ButtonWrapper.mock-data.ts
+++ src/hztl-foundation/src/stories/helpers/ButtonWrapepr/ButtonWrapper.mock-data.ts
@@ -1,7 +1,7 @@
 const defaultData = {
   id: 'buttonId',
-  children: 'Yo',
-  icon: 'icon',
+  children: 'Primary',
+  icon: '',
   ariaLabel: 'ariaLabel',
   title: 'title',
   className: 'bg-[#2F2D2E] py-3 px-4 text-white rounded',
@@ -9,6 +9,7 @@ const defaultData = {
 
 export const secondaryData = {
   ...defaultData,
+  children: 'Secondary',
   className: 'py-3 px-4 rounded border border-[#2F2D2E]',
 };
 
