422d9913d920b106a27558947ab08a812226d45e
Neelam <neelam.ramrakhiani.1@gmail.com>
2024-06-21 19:20:12 +0530

    removed comment code

--- src/hztl-foundation/src/stories/authorable/Hero/Hero.mock-data.ts
+++ src/hztl-foundation/src/stories/authorable/Hero/Hero.mock-data.ts
@@ -3,23 +3,6 @@ import { HeroProps } from '../../../components/authorable/shared/hztl-page-conte
 const defaultData: HeroProps = {
   rendering: { componentName: 'Default' },
   params: {},
-  // fields: {
-  //   Description: {
-  //     value:
-  //       '<span style="color: #27272a; background-color: #ffffff;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>',
-  //   },
-  //   Heading: {
-  //     value: 'Experience Forward',
-  //   },
-  //   Image: {
-  //     value: {
-  //       src: 'https://edge.sitecorecloud.io/horizontald8261-xmchztlfounf6cb-dev-2730/media/Project/HztlFoundation/SiteAlpha/placeholder-image.png?h=600&iar=0&w=800',
-  //       alt: 'wireframe',
-  //       width: '1200',
-  //       height: '750',
-  //     },
-  //   },
-  // },
   fields: {
     Description: {
       value:
