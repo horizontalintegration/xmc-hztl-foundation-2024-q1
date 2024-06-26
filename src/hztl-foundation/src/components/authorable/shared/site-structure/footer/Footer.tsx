9814289bf6292f7482de04ac2606474b4b8967cb
ShivamKuSingh <ssingh@horizontal.com>
2024-06-26 15:10:43 +0530

    Change css of footer navlinks

--- src/hztl-foundation/src/components/authorable/shared/site-structure/footer/Footer.tsx
+++ src/hztl-foundation/src/components/authorable/shared/site-structure/footer/Footer.tsx
@@ -45,9 +45,9 @@ export const Default = (props: FooterProps): JSX.Element => {
                           tag="h3"
                           field={{ value: groupLabel?.displayName }}
                         />
-                        <ul className="flex flex-col">
+                        <ul className="flex flex-col ml-0">
                           {links?.map((link, index) => (
-                            <li className="p-xxs" key={index}>
+                            <li className="p-xxs ml-0 list-none" key={index}>
                               <LinkWrapper
                                 className="font-modern text-gray text-xs font-bold capitalize"
                                 suppressNewTabIcon={true}
