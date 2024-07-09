0d6936c7032615739354a9b79e82c3f67ce94c12
Rahul Kumawat <rahulkumawat@Admins-MacBook-Pro.local>
2024-07-09 14:06:24 +0530

    integrating graphQl data

--- src/hztl-foundation/src/components/authorable/shared/site-structure/footer/Footer.tsx
+++ src/hztl-foundation/src/components/authorable/shared/site-structure/footer/Footer.tsx
@@ -19,6 +19,8 @@ const FooterDefaultComponent = (props: FooterProps): JSX.Element => (
 
 export const Default = (props: FooterProps): JSX.Element => {
   const id = props?.params?.RenderingIdentifier;
+  const footerColumns = props?.rendering?.fields?.data?.item?.footerColumns.items;
+  const footerLogo = props?.rendering?.fields?.data?.item.footerLogo;
   if (props?.fields) {
     return (
       <div
@@ -31,19 +33,18 @@ export const Default = (props: FooterProps): JSX.Element => {
           <div className="p-ml pb-s m-auto w-full max-w-screen-xl">
             <div className="flex flex-wrap justify-start md:justify-between gap-xl sm:gap-l md:gap-[50px] lg:gap-[62px] xl:gap-[162px] sm:w-[344px] md:w-auto">
               <div>
-                <ImageWrapper field={props?.fields?.footerLogo} />
+                <ImageWrapper field={{ value: footerLogo?.jsonValue.value }} />
               </div>
               <div className="flex justify-between flex-wrap gap-m sm:gap-ml mmd:gap-xl mml:gap-[140px] lg:gap-[204px] xl:gap-[216px] sm:w-[312px] md:w-auto">
-                {props?.fields?.footerColumns?.map((groupLabel, index) => {
-                  const links = groupLabel?.fields?.columnLinks as (Item &
-                    Data.Links.GenericLink)[];
+                {footerColumns?.map((groupLabel, index) => {
+                  const links = groupLabel?.columnLinks.items as (Item & Data.Links.GenericLink)[];
                   return (
                     <React.Fragment key={index}>
                       <div className="text-left">
                         <RichTextWrapper
                           className="font-modern text-gray text-xs font-bold capitalize"
                           tag="h3"
-                          field={{ value: groupLabel?.displayName }}
+                          field={{ value: groupLabel?.name }}
                         />
                         <ul className="flex flex-col">
                           {links?.map((link, index) => (
@@ -51,7 +52,7 @@ export const Default = (props: FooterProps): JSX.Element => {
                               <LinkWrapper
                                 className="font-modern text-gray text-xs font-bold capitalize"
                                 suppressNewTabIcon={true}
-                                field={link?.fields?.link}
+                                field={link?.link?.jsonValue}
                               />
                             </li>
                           ))}
