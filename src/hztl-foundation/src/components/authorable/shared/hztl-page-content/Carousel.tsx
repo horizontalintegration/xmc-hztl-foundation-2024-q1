dccc26d7e7b3d59a36a4547d3ec5aaea5a729de5
Rahul Kumawat <rahulkumawat@Admins-MacBook-Pro.local>
2024-07-16 15:16:38 +0530

    adding carousel item placeholder

---
+++ src/hztl-foundation/src/components/authorable/shared/hztl-page-content/Carousel.tsx
@@ -0,0 +1,75 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+
+// Lib
+import { ComponentProps } from 'lib/component-props';
+import { HztlPageContent } from '../../../../.generated/Feature.HztlFoundation.model';
+import { Splide, SplideTrack } from '@splidejs/react-splide';
+import '@splidejs/splide/css';
+
+// Helper
+import { SvgIcon } from 'helpers/SvgIconWrapper';
+import { Placeholder } from '@sitecore-jss/sitecore-jss-nextjs';
+
+export type CarouselProps = ComponentProps & HztlPageContent.CarouselItem;
+
+export const Default = (props: CarouselProps): JSX.Element => {
+  const id = props?.params?.RenderingIdentifier;
+  console.log(props);
+  const phKey = `carousel`;
+  return (
+    <div id={id ? id : undefined}>
+      <Splide
+        hasTrack={false}
+        options={{
+          rewind: true,
+          width: '100%',
+          gap: '.01rem',
+          perPage: 1,
+          perMove: 1,
+          pagination: true,
+          autoplay: 'pause',
+          interval: 3000,
+        }}
+      >
+        <SplideTrack>
+          <Placeholder name={phKey} rendering={props.rendering} />
+        </SplideTrack>
+
+        <div className="splide__arrows">
+          <button className="splide__arrow splide__arrow--prev icon-hover-focus-rounded max-lg:!top-auto max-lg:bottom-[100px] stroke-gray !bg-transparent">
+            <span className="sr-only">Previous slide</span>
+            <SvgIcon icon={'arrow-right'} />
+          </button>
+
+          <button className="splide__arrow splide__arrow--next icon-hover-focus-rounded max-lg:!top-auto max-lg:bottom-[100px] stroke-gray !bg-transparent">
+            <span className="sr-only">Next slide</span>
+            <SvgIcon icon={'arrow-right'} />
+          </button>
+        </div>
+
+        {/* Progress Bar */}
+        <div className="splide__progress">
+          <div className="splide__progress__bar" />
+        </div>
+
+        <div className="absolute bottom-1 right-5">
+          <button className="splide__toggle icon-hover-focus-rounded" type="button">
+            {/* Play button */}
+            <span className="splide__toggle__play">
+              <span className="sr-only">Play slideshow</span>
+              <SvgIcon className="h-l w-l" icon={'play'} />
+            </span>
+
+            {/* Pause button */}
+            <span className="splide__toggle__pause">
+              <span className="sr-only">Pause slideshow</span>
+              <SvgIcon className="h-l w-l" icon={'pause'} />
+            </span>
+          </button>
+        </div>
+      </Splide>
+    </div>
+  );
+};
+
---
+++ src/hztl-foundation/src/components/authorable/shared/hztl-page-content/CarouselItem.tsx
@@ -0,0 +1,78 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { ItemEx } from '../../../../.generated/_.Sitecore.Override';
+// Lib
+import { SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/css';
+
+// Helper
+import { withStandardComponentWrapper } from 'helpers/HOC';
+import LinkWrapper from 'helpers/SitecoreWrappers/LinkWrapper/LinkWrapper';
+import ImageWrapper from 'helpers/SitecoreWrappers/ImageWrapper/ImageWrapper';
+import { ComponentProps } from 'lib/component-props';
+import { HztlPageContent } from 'src/.generated/Feature.HztlFoundation.model';
+
+export type CarouselItemProps = ComponentProps & ItemEx & HztlPageContent.CarouselItem;
+
+const CarouselItemDefaultComponent = (props: CarouselItemProps): JSX.Element => {
+  return (
+    <div className={`component hero ${props?.params?.styles}`}>
+      <div className="component-content">
+        <span className="is-empty-hint">CarouselItem</span>
+      </div>
+    </div>
+  );
+};
+
+const CarouselItem = (props: CarouselItemProps): JSX.Element => {
+  if (props.fields) {
+    return (
+      <SplideSlide key={props.id}>
+        <div className="relative py-m w-[350px] mml:w-auto min-h-[500px] sm:h-auto md:h-[500px] mx-auto flex flex-col-reverse md:flex-row justify-center items-center">
+          {/* Slide Content. */}
+          <div className="slide-content z-10 lg:absolute lg:left-[3%] lg:top-1/2 lg:box-border lg:max-w-[70%] lg:-translate-y-1/2 lg:transform lg:bg-opacity-80 lg:p-5 lg:text-left xl:max-w-[50%]">
+            <div className="slide-content-inner">
+              <div className="relative flex flex-col gap-s p-6 sm:w-44 mmd:w-auto h-[225px]">
+                <h2 className="sm:text-5xl mmd:text-l capitalize font-bold">
+                  {props?.fields?.title?.value}
+                </h2>
+
+                {/* Slide description. */}
+                {props?.fields?.description && (
+                  <p className="text-xs text-gray">{props?.fields?.description?.value}</p>
+                )}
+
+                {/* Slide links. */}
+                {props?.fields?.primaryCTA && (
+                  <div className="flex">
+                    <div className="flex flex-col mml:flex-row gap-xs">
+                      <LinkWrapper
+                        field={props?.fields?.primaryCTA}
+                        aria-label={props?.fields?.primaryCTA?.value.text}
+                        className="w-32 h-12 rounded content-center text-center bg-gray text-white text-button font-bold"
+                      ></LinkWrapper>
+                      <LinkWrapper
+                        field={props?.fields?.secondaryCTA}
+                        aria-label={props?.fields?.secondaryCTA?.value.text}
+                        className="w-32 h-12 rounded content-center text-center text-button font-bold border-1 border-gray text-gray"
+                      ></LinkWrapper>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+
+          {/* Slide media. */}
+          <div className="slide-media">
+            <ImageWrapper field={props?.fields?.image} />
+          </div>
+        </div>
+      </SplideSlide>
+    );
+  }
+  return <CarouselItemDefaultComponent {...props} />;
+};
+
+export const Default = withStandardComponentWrapper(CarouselItem);
+
