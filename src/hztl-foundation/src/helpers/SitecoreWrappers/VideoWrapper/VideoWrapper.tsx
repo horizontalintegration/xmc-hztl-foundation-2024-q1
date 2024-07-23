ec44a4fee26ee1702a9747e11d0322caa7ffbdae
Eric Ranschau <eranschau@horizontal.com>
2024-07-23 15:27:08 -0500

    FIS-223: Updated Video Wrapper component.

--- src/hztl-foundation/src/helpers/SitecoreWrappers/VideoWrapper/VideoWrapper.tsx
+++ src/hztl-foundation/src/helpers/SitecoreWrappers/VideoWrapper/VideoWrapper.tsx
@@ -1,41 +1,130 @@
-import React from 'react';
+// Global
+import React, { useEffect } from 'react';
+import videojs from 'video.js';
+import 'videojs-youtube';
+import Player from 'video.js/dist/types/player';
+import 'video.js/dist/video-js.css';
 
-import { HztlPageContent } from 'src/.generated/Feature.HztlFoundation.model';
-import { ComponentProps } from 'lib/component-props';
+type Source = {
+  src: string;
+  type: string;
+};
 
-export type VideoWrapperProps = ComponentProps & HztlPageContent.Video;
+type Track = {
+  isDefault: boolean;
+  kind: string;
+  label: string;
+  srclang: string;
+  src: string;
+};
 
-const VideoWrapper = (props: VideoWrapperProps): JSX.Element => {
-  const uploadedVideoLink = props?.fields?.SelectFile?.value?.src;
-  const videoLink = props?.fields?.Title?.value;
-  const youtubeData = props?.fields?.YoutubeId?.value || '';
-  const autoPlayParameter = props?.fields || '';
-  const loopParameter = props?.fields || '';
-  const youtubeUrl = `https://www.youtube.com/embed/${youtubeData}?&loop=${
-    loopParameter ? 1 : 0
-  }&autoplay=${autoPlayParameter ? 1 : 0}`;
+type VideoJSProps = {
+  onReady?: (player: Player) => void;
+  options: {
+    autoplay?: boolean;
+    controls?: boolean;
+    fluid?: boolean;
+    height?: number | string;
+    loop?: boolean;
+    muted?: boolean;
+    poster?: string;
+    sources: Source[];
+    tracks: Track[];
+    width?: number | string;
+    youtubeVideoId?: string;
+  };
+};
+
+export type VideoWrapperProps = {
+  autoplay?: boolean;
+  captions?: Track[];
+  controls?: boolean;
+  fluid?: boolean;
+  height?: number | string;
+  loop?: boolean;
+  muted?: boolean;
+  poster?: string;
+  sources: Source[];
+  subtitles?: Track[];
+  width?: number | string;
+};
+
+const VideoJS = (props: VideoJSProps) => {
+  const playerRef = React.useRef<Player | null>(null);
+  const videoRef = React.useRef<HTMLDivElement | null>(null);
+
+  const { options, onReady } = props;
+
+  // Initialize the Video.js player once and only once.
+  useEffect(() => {
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
+      const videoElement = document.createElement('video-js');
+      videoElement.classList.add('vjs-big-play-centered');
+
+      videoRef.current?.appendChild(videoElement);
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        videojs.log('player is ready');
+        onReady && onReady(player);
+      }));
+    } else {
+      const player = playerRef.current;
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+    }
+  }, [onReady, options, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
 
   return (
-    <div tabIndex={0}>
-      {uploadedVideoLink ? (
-        <video width={900} height={500} controls>
-          <source src={uploadedVideoLink} type="video/mp4" />
-          <source src={uploadedVideoLink} type="video/ogg" />
-          Your browser does not support the video tag.
-        </video>
-      ) : (
-        <iframe
-          id={'videoId.value'}
-          name="youtubeVideo"
-          width={'100%'}
-          height={500}
-          src={youtubeUrl}
-          title={videoLink}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      )}
+    <div data-vjs-player>
+      <div ref={videoRef} />
     </div>
+  );
+};
+
+const VideoWrapper = (props: VideoWrapperProps): JSX.Element => {
+  const {
+    autoplay = false,
+    captions = [],
+    controls = true,
+    fluid = true,
+    height = 'auto',
+    loop = false,
+    muted = false,
+    poster,
+    sources,
+    subtitles = [],
+    width = '100%',
+  } = props || {};
+
+  return (
+    <VideoJS
+      options={{
+        autoplay,
+        controls,
+        fluid,
+        height,
+        loop,
+        muted,
+        poster,
+        sources,
+        tracks: [...captions, ...subtitles],
+        width,
+      }}
+    />
   );
 };
 
