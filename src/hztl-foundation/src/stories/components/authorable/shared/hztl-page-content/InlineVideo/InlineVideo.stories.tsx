80f58a8ecb605323aa13207357e7ff1733d72095
Eric Ranschau <eranschau@horizontal.com>
2024-07-31 15:38:59 -0500

    FIS-223: Storybook clean-up.

--- src/hztl-foundation/src/stories/components/authorable/shared/hztl-page-content/InlineVideo/InlineVideo.stories.tsx
+++ src/hztl-foundation/src/stories/components/authorable/shared/hztl-page-content/InlineVideo/InlineVideo.stories.tsx
@@ -14,48 +14,103 @@ const meta: Meta<InlineVideoProps> = {
   argTypes: {
     'fields.Video.value.autoplay': {
       description: 'A flag indicating wether or not the video should begin playing automatically.',
+      name: 'autoplay',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.captions': {
       description:
         'An array of URIs indicating localized caption data to be loaded into the player.',
+      name: 'captions',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.controls': {
       defaultValue: true,
       description: 'A flag indicating wether or not to show the playback controls.',
+      name: 'controls',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.fluid': {
       description:
         "A flag indicating wether or not the player will size to fit its container at the video's intrinsic aspect ratio.",
+      name: 'fluid',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.height': {
       description: 'A value indicating the desired height of the video player.',
       if: { arg: 'fields.Video.value.fluid', truthy: false },
+      name: 'height',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.loop': {
       defaultValue: false,
       description:
         'A flag indicating wether or not the video will loop automatically once playback has ended.',
+      name: 'loop',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.muted': {
       defaultValue: false,
       description: 'A flag indicated wether or not the video player is muted.',
+      name: 'muted',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.poster': {
       defaultValue: false,
       description:
         'A URI represenging a poster image of the video asset to be loaded into the player.',
+      name: 'poster',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.sources': {
       description:
         'An array of URIs and asset types representing a video asset to be loaded into the player.',
+      name: 'sources',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.subtitles': {
       description:
         'An array of URIs indicating localized caption data to be loaded into the player.',
+      name: 'subtitles',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     'fields.Video.value.width': {
       description: 'A value indicating the desired height of the video player.',
       if: { arg: 'fields.Video.value.fluid', truthy: false },
+      name: 'width',
+      table: {
+        category: 'fields',
+        subcategory: 'Video',
+      },
     },
     /* eslint-disable  @typescript-eslint/no-explicit-any */
   } as any,
