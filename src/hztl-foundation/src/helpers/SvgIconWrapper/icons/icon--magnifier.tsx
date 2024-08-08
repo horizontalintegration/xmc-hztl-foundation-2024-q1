f323a99bc7cace57087a068b9ac3fb61e95cd02a
jay-hztl <jthakkar@horizontal.com>
2024-08-08 16:06:47 +0530

    Add accordian collapse and expand icon changes | Replace SVG Icon to SVG component | Add Refine by button for mobile menu | Rename BurgurIcon to Hamburger | Pagination design related changes

--- src/hztl-foundation/src/helpers/SvgIconWrapper/icons/icon--magnifier.tsx
+++ src/hztl-foundation/src/helpers/SvgIconWrapper/icons/icon--magnifier.tsx
@@ -2,10 +2,15 @@
 const IconMagnifier = (): JSX.Element => {
   return (
     <>
-      <path xmlns="http://www.w3.org/2000/svg" fill="none" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="116.3636" stroke="#000" d="M320 64v256M320 320v256M320 320h256M320 320h-256"/>
+      <path
+        d="M14.75 14.75L10.25 10.25M11.75 6.5C11.75 9.39949 9.39949 11.75 6.5 11.75C3.60051 11.75 1.25 9.39949 1.25 6.5C1.25 3.60051 3.60051 1.25 6.5 1.25C9.39949 1.25 11.75 3.60051 11.75 6.5Z"
+        stroke="#2F2D2E"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
     </>
   );
 };
 
 export default IconMagnifier;
-
