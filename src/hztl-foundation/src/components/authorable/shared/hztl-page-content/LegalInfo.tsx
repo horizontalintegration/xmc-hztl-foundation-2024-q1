1a9b3bd081de3a3763cad4c8d12e700b6fc238ae
Eric Ranschau <eranschau@horizontal.com>
2024-09-17 13:35:02 -0500

    FIS-297: Updated Legal Info component.

--- src/hztl-foundation/src/components/authorable/shared/hztl-page-content/LegalInfo.tsx
+++ src/hztl-foundation/src/components/authorable/shared/hztl-page-content/LegalInfo.tsx
@@ -6,56 +6,64 @@ import { ComponentProps } from 'lib/component-props';
 import { SiteStructure } from 'src/.generated/Feature.HztlFoundation.model';
 
 // Local
-import RichTextWrapper from 'helpers/SitecoreWrappers/RichTextWrapper/RichTextWrapper';
 import LinkWrapper from 'helpers/SitecoreWrappers/LinkWrapper/LinkWrapper';
+import RichTextWrapper from 'helpers/SitecoreWrappers/RichTextWrapper/RichTextWrapper';
 
-// Types
-export type LegalInfoProps = ComponentProps & SiteStructure.Footer.LegalInfo;
-
-/*
- * Tailwind Variants
- */
-
-const tailwindVariants = tv({
+const TAILWIND_VARIANTS = tv({
   slots: {
-    base: ['flex', 'justify-start', 'col-span-2', 'mmd:col-span-1'],
-    wrapper: [
-      'w-full',
-      'flex',
-      'max-w-[724px]',
-      'py-4',
-      'px-spacing-spacing-4',
-      'items-center',
-      'justify-center',
-      'mmd:justify-end',
-    ],
+    base: ['col-span-2', 'flex', 'justify-start', 'mmd:col-span-1'],
     content: [
-      'flex',
       'flex-col',
-      'gap-xxs',
+      'flex',
+      'gap-2',
+      'items-center',
       'text-center',
       'lg:text-left',
-      'items-center',
       'ml:items-end',
     ],
-    textWrapper: ['font-modern', 'text-black', 'text-xs', 'font-regular'],
-    legalMenuWrapper: ['flex'],
+    divider: ['bg-theme-black', 'h-8', 'w-px'],
     legalMenuItems: ['flex', 'items-center'],
-    linkWrapper: [
-      'p-xxs',
-      'font-modern',
-      'text-center',
-      'text-black',
-      'text-text-link',
-      'font-bold',
+    legalMenuWrapper: ['flex'],
+    linkWrapper: ['font-bold', 'font-modern', 'p-2', 'text-theme-black', 'text-center', 'text-sm'],
+    textWrapper: ['font-modern', 'font-regular', 'text-theme-black', 'text-xs'],
+    wrapper: [
+      'flex',
+      'items-center',
+      'justify-center',
+      'max-w-[724px]',
+      'px-spacing-spacing-4',
+      'py-4',
+      'w-full',
+      'mmd:justify-end',
     ],
-    divider: ['h-ml w-px bg-black'],
   },
 });
 
+type LegalMenuItem = {
+  displayName: string;
+  fields: {
+    legalLink: {
+      value: {
+        anchor?: string;
+        class?: string;
+        href?: string;
+        id?: string;
+        linktype?: string;
+        querystring?: string;
+        target?: string;
+        text?: string;
+        title?: string;
+      };
+    };
+  };
+  name: string;
+};
+
+export type LegalInfoProps = ComponentProps & SiteStructure.Footer.LegalInfo;
+
 export const Default = (props: LegalInfoProps): JSX.Element => {
-  const id = props?.params?.RenderingIdentifier;
-  const legalMenu = props?.fields?.legalMenu || [];
+  const { copyrightText, legalMenu } = props?.fields || {};
+  const { RenderingIdentifier } = props?.params || {};
 
   const {
     base,
@@ -66,7 +74,7 @@ export const Default = (props: LegalInfoProps): JSX.Element => {
     legalMenuWrapper,
     legalMenuItems,
     divider,
-  } = tailwindVariants();
+  } = TAILWIND_VARIANTS();
 
   /*
    * Rendering
@@ -79,29 +87,27 @@ export const Default = (props: LegalInfoProps): JSX.Element => {
   return (
     <div
       className={`${base()} ${props?.params?.styles !== undefined ? props?.params?.styles : ''}`}
-      data-component="authorable/general/legal-info"
-      id={id}
+      data-component="authorable/shared/hztml-page-content/legalinfo"
+      id={RenderingIdentifier}
     >
       <div className={wrapper()}>
         <div className={content()}>
-          <RichTextWrapper
-            className={textWrapper()}
-            field={props?.fields?.copyrightText}
-            tag="div"
-          />
+          <RichTextWrapper className={textWrapper()} field={copyrightText} tag="div" />
           <div className={legalMenuWrapper()}>
-            {props?.fields?.legalMenu?.map((navLink, index) => (
-              <div key={index} className={legalMenuItems()}>
-                <LinkWrapper
-                  className={linkWrapper()}
-                  field={{
-                    ...navLink?.fields?.legalLink,
-                  }}
-                  suppressNewTabIcon={true}
-                />
-                {legalMenu?.length > index + 1 && <div className={divider()}></div>}
-              </div>
-            ))}
+            {legalMenu?.map((legalMenuItem: LegalMenuItem, index) => {
+              const { legalLink } = legalMenuItem?.fields || {};
+
+              return (
+                <div key={legalLink?.value?.id} className={legalMenuItems()}>
+                  <LinkWrapper
+                    className={linkWrapper()}
+                    field={{ ...legalLink }}
+                    suppressNewTabIcon={true}
+                  />
+                  {legalMenu?.length > index + 1 && <div className={divider()}></div>}
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
