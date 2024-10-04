7d88a02764c2510fa6f980376c5781008fb689bb
laphinh sonexaythiketh <lsonexaythiketh@horizontal.com>
2024-10-04 11:23:08 -0500

    FIS-376: Removed next/link and use anchor tag

--- src/hztl-foundation/src/components/SkipNav.tsx
+++ src/hztl-foundation/src/components/SkipNav.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import SkipNavHelper from './SkipNavHelper';
-import Link from 'next/link';
 
 const SkipNav: React.FC = () => {
   return (
     <div className="relative">
-      <Link
-        href="/#content"
+      <a
+        href="#content"
         title="Skip to the main content"
         className="sr-only focus:not-sr-only focus:absolute focus:top-0 focus:left-0 focus:w-full focus:bg-black focus:text-white focus:p-2 focus:z-50 focus:flex focus:justify-center"
       >
         Skip to main content
-      </Link>
+      </a>
       <SkipNavHelper />
     </div>
   );
--- src/hztl-foundation/src/components/SkipNavHelper.tsx
+++ src/hztl-foundation/src/components/SkipNavHelper.tsx
@@ -4,16 +4,28 @@ import { useEffect } from 'react';
 const SkipNavHelper: React.FC = () => {
   useEffect(() => {
     const handleKeydown = (event: KeyboardEvent) => {
-      const skipLink = document.querySelector('a[href="/#content"]');
+      const skipLink = document.querySelector('a[href="#content"]');
       if (event.key === ' ' && skipLink && skipLink === document.activeElement) {
         event.preventDefault();
         (skipLink as HTMLAnchorElement).click();
       }
     };
 
+    const handleClick = () => {
+      const contentElement = document.getElementById('content');
+      if (contentElement) {
+        contentElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        contentElement.focus(); // Ensure the content is focused
+      }
+    };
+
+    const skipLink = document.querySelector('a[href="#content"]');
+    skipLink?.addEventListener('click', handleClick);
+
     document.addEventListener('keydown', handleKeydown);
 
     return () => {
+      skipLink?.removeEventListener('click', handleClick);
       document.removeEventListener('keydown', handleKeydown);
     };
   }, []);
