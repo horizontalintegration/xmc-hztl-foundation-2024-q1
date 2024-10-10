3bd7b8254aadd952dfb93f68077a3017b790c37c
Eric Ranschau <eranschau@horizontal.com>
2024-10-10 11:19:32 -0500

    FIS-365: Upgrade to NextJS 21.7.0.

--- src/hztl-foundation/src/lib/page-props-factory/plugins/content-styles.ts
+++ src/hztl-foundation/src/lib/page-props-factory/plugins/content-styles.ts
@@ -1,5 +1,6 @@
 import { SitecorePageProps } from 'lib/page-props';
 import { getContentStylesheetLink } from '@sitecore-jss/sitecore-jss-nextjs';
+import { constantCase } from 'constant-case';
 import { Plugin } from '..';
 
 class ContentStylesPlugin implements Plugin {
@@ -7,7 +8,11 @@ class ContentStylesPlugin implements Plugin {
 
   async exec(props: SitecorePageProps) {
     // Get content stylessheet link, empty if styles are not used on the page
-    const contentStyles = getContentStylesheetLink(props.layoutData);
+    const sitecoreEdgeContextId = process.env[`${constantCase('sitecoreEdgeContextId')}`];
+    const contentStyles = getContentStylesheetLink(
+      props.layoutData,
+      sitecoreEdgeContextId as string
+    );
 
     contentStyles && props.headLinks.push(contentStyles);
 
